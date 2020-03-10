1680dcea2cc7ed8c35d7af7e4fa098da12bdd72e
zy499 <hqh499@gmail.com>
2020-03-10 16:33:17 +0800

    up

--- src/packages/index.js
+++ src/packages/index.js
@@ -12,9 +12,9 @@ const install = (Vue) => {
 
 // ! 有可能组件会通过script标签的方式引入 <script src="zy-ui"></script>
 // todo 为了防止这种引入方式 自动触发 install方法
-if (typeof window.Vue !== 'undefined') {
-  install(Vue) // ? 全局直接通过script 引用的方式会默认调用install方法
-}
+// if (typeof window.Vue !== 'undefined') {
+//   install(Vue) // ? 全局直接通过script 引用的方式会默认调用install方法
+// }
 
 export default {
   install
